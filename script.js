d6242acdcbea0d919d5118b1f164c0a4d0dd5fbb
arslonga83 <jlwood83@gmail.com>
2022-07-29 22:40:45 -0400

    added checkboxes for read status...temporarily in separate cells

--- script.js
+++ script.js
@@ -18,33 +18,12 @@ function addBookToLibrary() {
     newBook.title = prompt("Title: ");
     newBook.author = prompt("Author: ");
     newBook.pages = prompt("Pages: ");
-    newBook.read = prompt("Read? ");
-    newBook.data = myLibrary.length - 1;
+    newBook.read = prompt("Read? (type 0 for no and 1 for yes)");
     myLibrary.push(newBook);
-    //populateRow();
     document.querySelector('#tableBody').innerHTML = "";
     makeTable(myLibrary);
     console.table(myLibrary);
 }
-
-//PRETTY SURE I CAN GET RID OF THIS FUNCTION NOW
-//specifically adds the newest book to table
-////function populateRow() {
-//    let table = document.querySelector('table');
-  //  let newRow = table.insertRow();
-  //  for (i = myLibrary.length - 1, j = 0; j < 4; j++) {
-  //      let newCell = newRow.insertCell();
-  //      let newText = document.createTextNode(Object.values(myLibrary[i])[j]);
-  //      newCell.appendChild(newText);
-  //  }
-  //  newCell = newRow.insertCell();
-  //  let button = document.createElement('button');
-  //  button.dataset.index = myLibrary.length - 1;
-  //  newRow.setAttribute('id', myLibrary.length - 1);
-  //  button.classList.add('deleteButton');
-  //  newCell.appendChild(button);
-  //  createDelete();
-//}
 
 // this function make the table from myLibrary
 function makeTable(myLibrary) {   
@@ -58,20 +37,28 @@ function makeTable(myLibrary) {
             newCell.appendChild(newText);
         }
         newCell = newRow.insertCell();
+        let checkbox = document.createElement('input');
+        checkbox.setAttribute('type', 'checkbox');
+        if (Object.values(myLibrary[i])[3] == true) {
+            checkbox.checked = true;
+        }
+        newCell.appendChild(checkbox);
+        newCell = newRow.insertCell();
         newCell.classList.add('buttonCell');
         let button = document.createElement('button');
         button.dataset.index = i;
         button.classList.add('deleteButton');
         newCell.appendChild(button);
+        
     }
     createDelete();
     console.table(myLibrary);
 }
 
 //manually inserted some books to figure out display
-myLibrary.push(new Book('The Hobbit', 'J. R. R. Tolkien', 295, 'not read yet'));
-myLibrary.push(new Book('Narwhal and Jelly', 'Ben Clanton', 40, 'read'));
-myLibrary.push(new Book('Elephant and Piggie', 'Mo Willems', 45, 'read'));
+myLibrary.push(new Book('The Hobbit', 'J. R. R. Tolkien', 295, false));
+myLibrary.push(new Book('Narwhal and Jelly', 'Ben Clanton', 40, true));
+myLibrary.push(new Book('Elephant and Piggie', 'Mo Willems', 45, true));
 
 makeTable(myLibrary);
 
